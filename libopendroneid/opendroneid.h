a6bbd4ce9baf06682c24b7ac4eeeb386852c0496
Simon Wunderlich <sw@simonwunderlich.de>
2019-05-21 15:58:59 +0200

    libopendroneid: add OpenDroneID Wifi implementation calls for building
    Signed-off-by: Simon Wunderlich <sw@simonwunderlich.de>

--- libopendroneid/opendroneid.h
+++ libopendroneid/opendroneid.h
@@ -11,6 +11,7 @@ gabriel.c.cox@intel.com
 */
 
 #include <stdint.h>
+#include <string.h>
 #define ODID_MESSAGE_SIZE 25
 #define ODID_ID_SIZE 20
 #define ODID_STR_SIZE 23
@@ -363,6 +364,67 @@ char *safe_copyfill(char *dstStr, const char *srcStr, int dstSize);
 char *safe_dec_copyfill(char *dstStr, const char *srcStr, int dstSize);
 int intRangeMax(int64_t inValue, int startRange, int endRange);
 int intInRange(int inValue, int startRange, int endRange);
+
+// OpenDroneID WiFi functions
+
+/**
+ * odid_message_encode_pack - encodes the messages in the odid pack
+ * @inPack: plain data message pack
+ * @pack: buffer space to write to
+ * @buflen: maximum length of buffer space
+ *
+ * Returns length on success, < 0 on failure. @buf only contains a valid message
+ * if the return code is >0
+ */
+int odid_message_encode_pack(ODID_UAS_Data *UAS_Data, void *pack, size_t buflen);
+
+/* odid_wifi_build_message_pack_nan_action_frame - creates a message pack
+ * with each type of message from the drone information into an NAN action fram
+ * @UAS_Data: general drone status information
+ * @mac: mac address of the wifi adapter where the NAN frame will be sent
+ * @send_counter: sequence number, to be increase for each call of this function
+ * @buf: pointer to buffer space where the NAN will be written to
+ * @buf_size: maximum size of the buffer
+ *
+ * Returns the packet length on success, or < 0 on error.
+ */
+int odid_wifi_build_message_pack_nan_action_frame(ODID_UAS_Data *UAS_Data, char *mac,
+						  uint8_t send_counter,
+						  uint8_t *buf, size_t buf_size);
+
+/**
+* IEEE 802.11 structs to build management action frame
+*/
+struct __attribute__((__packed__)) ieee80211_mgmt {
+	uint16_t frame_control;
+	uint16_t duration;
+	uint8_t da[6];
+	uint8_t sa[6];
+	uint8_t bssid[6];
+	uint16_t seq_ctrl;
+};
+
+struct __attribute__((__packed__)) nan_service_discovery {
+	uint8_t category;
+	uint8_t action_code;
+	uint8_t oui[3];
+	uint8_t oui_type;
+};
+
+struct __attribute__((__packed__)) nan_service_descriptor_attribute {
+	uint8_t attribute_id;
+	uint16_t length;
+	uint8_t service_id[6];
+	uint8_t instance_id;
+	uint8_t requestor_instance_id;
+	uint8_t service_control;
+	uint8_t service_info_length;
+};
+
+struct __attribute__((__packed__)) ODID_service_info {
+	uint8_t message_counter;
+	ODID_Message_Pack odid_message_pack[];
+};
 
 #ifndef ODID_DISABLE_PRINTF
 void printByteArray(uint8_t *byteArray, uint16_t asize, int spaced);
